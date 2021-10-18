4bc2a0ef1b6861ae51561c1034e8f0c1aa8972bd
chaeeun-seo <77956939+chaeeun-seo@users.noreply.github.com>
2021-10-18 16:40:25 +0900

    Create JavaScript:2.Variables.js
    1. learn how console.log() works
    2. make a simple calculator and understand why we need variables
    3. use variables to make a simple calculator
    4. learn the rules of making variable name in JS and Python: camelCase, snake_case

---
+++ JavaScript:2.Variables.js
@@ -0,0 +1,36 @@
+// learn javascript Variables
+
+// learn how console.log() works
+console.log(565656)
+console.log("hello")
+console.log('hi')
+
+// make simple calculator
+console.log(8 + 5);
+console.log(8 * 5);
+console.log(8 / 5);
+
+// if I want to change the numbers above, I've got to change one by one
+// I can make it easy if I use variables
+// job of variable is to save or hold a value
+// use const(constant) to make a non-changing variable
+
+const a = 8;
+const b = 5;
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+
+// I have to change only two things to change the numbers
+
+// rules of making variable's name
+// 1. variable's name cannot have spaces
+// 2. camelCase: in JavaScript, if a word needs space, uppercase the letter
+
+const myName = "something"
+console.log("hello " + myName) 
+const veryLongVariableName = 0;
+
+// snake_case : in python, if a word needs space, put underbar(_) between
+// very_long_variable_name = 0;
