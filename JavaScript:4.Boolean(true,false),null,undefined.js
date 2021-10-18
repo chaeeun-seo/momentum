9599128bcf383f05a76b3d48055eb3506b7aef93
chaeeun-seo <77956939+chaeeun-seo@users.noreply.github.com>
2021-10-18 21:35:59 +0900

    Create JavaScript:4.Boolean(true,false),null,undefined.js
    * Boolean(true, false)
    * null : nothing / there is value but nothing
    * undefined : something exists but has no value
    * comparison between JS and Python

---
+++ JavaScript:4.Boolean(true,false),null,undefined.js
@@ -0,0 +1,39 @@
+// learn Boolean (true, false)
+
+const amIFat = true;
+console.log(amIFat);
+const amISkinny = false;
+console.log(amISkinny);
+
+// when do we use Boolean
+// EX. when we want to check if a user logged in or not
+// EX. is the video playing
+// EX. is the website loaded
+
+
+
+// learn null, undefined
+// Data Types that are non-existing, undefined, nothing
+
+// 1. null : nothing
+// there is value but nothing
+// null never happens naturally
+// null happens when we make sure that something doesn't exist in the variable
+// use null when I want to tell JS that this value is literally nothing
+const amISmart = null;
+console.log(amISmart);
+
+// 2. undefined : something exists but no value
+// variable exists but is not defined
+// variable exists in the computer memory
+// there is space in the memory of the computer but is not defined 
+// variable is created, computer is aware of that variable but it doesn't have a value
+let something;
+console.log(something);
+
+
+
+// Comparison between JS and Python
+// JS(null) VS. Python(None)
+// JS(true) VS. Python(True)
+// JS(false) VS. Python(False)
