839326fee0fe20b95cedcc8fdbf6e350d6089fbc
chaeeun-seo <77956939+chaeeun-seo@users.noreply.github.com>
2021-10-28 16:42:20 +0900

    Create JavaScript:15.TheDocumentObject.js
    * what is document and how I can use document
    * document.propertyName : able to get things from html by using javascript
    * document.propertyName = value : able to update/change things from html by using javascript

---
+++ JavaScript:15.TheDocumentObject.js
@@ -0,0 +1,29 @@
+// The Document Object
+
+// if I write 'document" on the console, I can get the html
+// 'document" is some sort of object that is already included in the browser
+// 'document" refers to the html 
+
+// 'document" is just an object
+// 'document" is the representation of the html from the point of view of javascript
+console.dir(document);
+
+// able to get properties of an object
+// able to get title of html by document by javascript
+// javascript is already configured to be able to read and access html
+// browser gives us the document object that has a lot of info about 
+// we are looking at html code from the point of view of javascript
+console.log(document.title);
+
+// able to set properties of an object
+// able to change html by javascript
+document.title = "Hello";
+console.log(document.title);
+
+// javascript is already connected to html
+// able to get things from the html
+// able to look at them from javascript code
+
+console.log(document.body)
+
+// able to add something to the html
