d98a5212524d260f60e1f3527594b75ee123fd81
chaeeun-seo <77956939+chaeeun-seo@users.noreply.github.com>
2022-01-09 13:07:06 +0900

    Create JavaScript19:document.getElementsByTagName("tagName").js
    To understand how to use document.getElementsByTagName("tagName");
    >> able to get elements by tagname
    >> we can get an array of elements whose tag name matches

---
+++ JavaScript19:document.getElementsByTagName("tagName").js
@@ -0,0 +1,9 @@
+// Searching For Elements
+
+// document.getElementsByTagName("tagName");
+// able to get elements by tagname
+// we can get an array of elements whose tag name matches
+
+const title = document.getElementsByTagName("h1");
+
+console.log(title);
