2adf804e9329b295d24553a2b6b5a185103e8bf3
chaeeun-seo <77956939+chaeeun-seo@users.noreply.github.com>
2022-01-09 12:58:17 +0900

    Create JavaScript18:document.getElementsByClassName("className").js
    To understand how we use document.getElementsByClassName("className");
    >> use when we want to select many elements at the same time using class 
    >> we can get array of elements whose classname matches

---
+++ JavaScript18:document.getElementsByClassName("className").js
@@ -0,0 +1,9 @@
+// Searching For Elements
+
+// document.getElementsByClassName("className");
+// to select many elements at the same time using class 
+// we can get array of elements whose classname matches
+
+const hellos = document.getElementsByClassName("hello");
+
+console.log(hellos);
