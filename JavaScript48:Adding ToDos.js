0059eee91f59487154718c4851a8843fde41a24c
chaeeun-seo <77956939+chaeeun-seo@users.noreply.github.com>
2022-01-11 22:47:47 +0900

    Create JavaScript48:Adding ToDos.js
    Adding ToDos
    
    [function paintToDo(newTodo){}]
    * get newTodo string and put it in the span's innerText
    * make span as li's child and make li as toDoList's child
    
    [function handleToDoSubmit(event){}]
    * whenever toDoForm is submitted, handleToDoSubmit is executed
    * stops default of form when being submitted and save input's value in variable newTodo
    * make input empty
    * give paintTodo function newTodo as argument to add new todo to the todo list

---
+++ JavaScript48:Adding ToDos.js
@@ -0,0 +1,27 @@
+// Adding ToDos
+
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+// const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  paintToDo(newTodo);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+// tasks
+// 1. I want to delete todo
+// 2. If I refresh, my todos are lost
