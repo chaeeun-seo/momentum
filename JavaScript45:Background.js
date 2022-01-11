d7cad0808093bc4314fe72b7dd4939e84f8a7039
chaeeun-seo <77956939+chaeeun-seo@users.noreply.github.com>
2022-01-11 22:02:43 +0900

    Create JavaScript45:Background.js
    Background
    
    [connect js file]
    * by using script tag, I connected "js/background.js" to html
    
    [document.createElement("")]
    * create sth in js and put that on html
    
    [document.body.appendChild()]
    * add html to the body
    >> add bgImage inside of body

---
+++ JavaScript45:Background.js
@@ -0,0 +1,15 @@
+// Background
+
+const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+// document.createElement("")
+// create sth in js and put that on html
+const bgImage = document.createElement("img");
+
+bgImage.src = "img/${chosenImage}";
+
+// document.body.appendChild() : add html to the body
+// add bgImage inside of body
+document.body.appendChild(bgImage);
