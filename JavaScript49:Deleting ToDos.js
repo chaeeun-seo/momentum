4e142f2293f44537c65e88c31811f12d433a3ffe
chaeeun-seo <77956939+chaeeun-seo@users.noreply.github.com>
2022-01-11 23:06:52 +0900

    Create JavaScript49:Deleting ToDos.js
    Deleting ToDos
    
    [Deleting ToDos]
    * add a button that will delete the todo
    
    [function painToDo(newTodo){}]
    * create a button using createElement
    * set innerText of button ❌ using [window] + .
    * add eventListener to button
    >> whenever click event happens, execute deleteToDo function
    
    [function deleteToDo(event){}]
    * find parentElement of button >> li
    * use element.remove() to remove

---
+++ JavaScript49:Deleting ToDos.js
@@ -0,0 +1,37 @@
+// Deleting ToDos
+// add a button that will delete the todo
+
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+// const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+// target is html element that was clicked
+// all html elements have one html property : parentElement
+// parentElement is the parent of the element that was clicked
+
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  paintToDo(newTodo);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
