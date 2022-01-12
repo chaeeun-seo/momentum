cfca08e5ac8624fab7ec3c1335d7ed67b96ac3d5
chaeeun-seo <77956939+chaeeun-seo@users.noreply.github.com>
2022-01-12 20:38:57 +0900

    Create JavaScript55:Deleting ToDos part 3.js
    Deleting ToDos part 3
    
    [deleteToDo(){}]
    *  we want to keep toDo whose id is not equals to li's id
    >>use filer to keep those whose id is not same as li's id 
    * id of li is string and id of toDo is number
    >> parseInt(li.id) to take string and make it into number

---
+++ JavaScript55:Deleting ToDos part 3.js
@@ -0,0 +1,69 @@
+// Deleting ToDos part 3
+
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+// we want to keep toDo whose id is not equals to li's id
+// id of li is string and id of toDo is number
+// parseInt(li.id) to take string and make it into number
+
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
+}
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  li.id = newTodo.id;
+  const span = document.createElement("span");
+  span.innerText = newTodo.mood;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  const newTodoObj = {
+    mood: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
+
+// array.filter(function)
+// if we want to delete sth from the array, we don't go to array and delete sth
+// what we actually do is to create a new array without the item that we want to delete
+// we don't delete item, we exclude the item
+// old array will stay and we create a new array without item that we want to delete
+// the function in the filter should return true if I want to keep the object in the new array
+// if the function returns false, that item will not be included in the new array
+// the function should return true, if the item should stay
